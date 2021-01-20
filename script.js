ae7ca6798f229f69a1e08055547a8d9627b7c8fb
Ayanna Aziz <ayanna.aziz77@gmail.com>
2021-01-20 18:27:06 -0500

    all prompts work and show up on screen

--- script.js
+++ script.js
@@ -21,15 +21,32 @@ var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var passwordLength = 0;
 
+
 // Declare vars for user to confirm in prompts
 var upperCase;
 var lowerCase;
-var number;
+var numeric;
 var specialChar;
+var passwordLength ;
  
 function generatePassword() {
 
   var passwordLength = (prompt("How many long would you like your password to be? Choose between 8-128."));
 
+  // Loop iff answer is outside the parameters 
+while(passwordLength <= 7 || passwordLength >= 129) {
+  alert("Password length must be between 8-128 characters Try again");
+  var passwordLength = (prompt("How many characters would you like your password to contain?"));
+  } 
+ // Determine parameters of password 
+ var specialChar = confirm("Click OK to include special characters");
+ var numeric = confirm("Click OK to include numeric characters");    
+ var lowerCase = confirm("Click OK to include lowercase characters");
+ var upperCase = confirm("Click OK to include uppercase characters");
 
+
+ 
 }
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
