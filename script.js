1fd5af8987562f483d450c3d2428e9379b209b3f
Ayanna Aziz <ayanna.aziz77@gmail.com>
2021-01-20 16:43:00 -0500

    added vard for confirmation prompts that will be shown to user

--- script.js
+++ script.js
@@ -19,6 +19,17 @@ var numChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var passwordLength = 0;
+
+// Declare vars for user to confirm in prompts
+var upperCase;
+var lowerCase;
+var number;
+var specialChar;
+ 
+function generatePassword() {
+
+  var passwordLength = (prompt("How many long would you like your password to be? Choose between 8-128."));
 
 
-
+}
