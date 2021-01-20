4d23f9fc5d0244acc5b2fce0b2ba52e0db6631bd
Ayanna Aziz <ayanna.aziz77@gmail.com>
2021-01-20 16:16:14 -0500

    added variables that will be used to generate password

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Create generatepassword function
+// Create generatepassword function
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -11,5 +11,14 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+// CODE HERE
+
+// Creating variables with array of characters that can be used to generate password
+var numChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+
+
